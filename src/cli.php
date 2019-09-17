fbc896af95567792b195414b2734a7fa0efe009c
l33tLaserdance <disruptenemyconnection@yahoo.com>
2019-09-17 11:26:25 +0300

    run()

--- src/cli.php
+++ src/cli.php
@@ -9,6 +9,4 @@ function run()
     line('Welcome to the Brain Game!');
     $name = \cli\prompt('May I have your name?');
     line("Hello, %s!", $name);
-    \cli\Colors::enable(); // Forcefully enable
-    \cli\line('  %C%3All output is run through %Y%2\cli\Colors::colorize%C%3 before display%n');
 }
