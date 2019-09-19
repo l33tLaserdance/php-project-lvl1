35511a452241958823ef50cf44c481842dec7a0a
l33tLaserdance <disruptenemyconnection@yahoo.com>
2019-09-19 12:22:40 +0300

    even.php fixes

--- src/even.php
+++ src/even.php
@@ -8,10 +8,13 @@ function even()
 {
     \cli\line(' %w%8Welcome to Brain Games!%n');
     \cli\line(' %w%8Answer "%g%8yes%w%8" if number is even, otherwise answer "%r%8no%w%8"%n');
-
     $name = \cli\prompt(' %w%8May I have your name?%n');
     \cli\line(' %g%8Hello, %s!%n', $name);
+    gameStart($name);
+}
 
+function gameStart($name)
+{
     $i = 0;
     for ($i = 0; $i < 3; $i++) {
         $number = rand(1, 30);
@@ -25,15 +28,18 @@ function even()
         \cli\line(' %w%8Question: %s%n', $number);
         $result = \cli\prompt(" ");
         \cli\line(" %w%8Your answer: %s%n", $result);
-        if ($result == $answer) {                                                                                                   if ($i != 2) {
+        if ($result == $answer) {
+            if ($i != 2) {
                 \cli\line(" %g%8Correct!%n");
             } else {
                 \cli\line(" %g%8Correct!%n");
                 return \cli\line(" %g%8Congratulations, %s!%n", $name);
             }
         } else {
-            \cli\line(" %w%8'%r%8{:a}%w%8' is wrong answer ;(. Correct answer was '%g%8{:b}%w%8'%n",
-        array('a' => $result, 'b' => $wrong));
+            \cli\line(
+                " %w%8'%r%8{:a}%w%8' is wrong answer ;(. Correct answer was '%g%8{:b}%w%8'%n",
+                array('a' => $result, 'b' => $wrong)
+            );
             return \cli\line(" %r%8Let's try again, %s!%n", $name);
         }
     }
